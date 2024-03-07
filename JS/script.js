5f797db8c6493148fe28ddc05727f10240463643
Victor-Lis <victorlisbronzo1@gmail.com>
2024-03-07 20:08:55 -0300

    v1
    - Iniciando codificação do projeto

---
+++ JS/script.js
@@ -0,0 +1,51 @@
+const rockElement = document.querySelector("#rock")
+const paperElement = document.querySelector("#paper")
+const scissorElement = document.querySelector("#scissor")
+
+const playerSelectElement = document.querySelector("#player-select")
+const movesElement = document.querySelector("#moves")
+
+const playerMoveElement = document.querySelector("#playerMove")
+
+const playStyles = [
+{
+    type: "rock",
+    losesTo: "paper",
+    img: "./assets/rock.png"
+},
+{
+    type: "paper", 
+    losesTo: "scissor",
+    img: "./assets/paper.png"
+},
+{
+    type: "scissor",
+    losesTo: "rock",
+    img: "./assets/scissor.png"
+}
+]
+
+function play(jogada){
+    const playerMove = playStyles.find((style) => style.type == jogada)  
+    const randomMove = playStyles[Math.round(Math.random()*playStyles.length)]
+
+    playerSelectElement.classList.add("hidden")
+
+    playerMoveElement.children[0].src = playerMove.img
+    playerMoveElement.children[1].innerText = playerMove.type
+
+    movesElement.classList.remove("hidden")
+}
+
+rockElement.addEventListener("click", (e) => {
+    play("rock")
+})
+
+paperElement.addEventListener("click", (e) => {
+    play("paper")
+})
+
+scissorElement.addEventListener("click", (e) => {
+    play("scissor")
+})
+
